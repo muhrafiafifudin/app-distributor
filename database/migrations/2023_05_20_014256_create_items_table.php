c2c129b1545b29e6714dbd44301dbb1a023fa813
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-05-20 08:59:29 +0700

    Add database

---
+++ database/migrations/2023_05_20_014256_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code');
+            $table->string('name');
+            $table->string('image');
+            $table->tinyInteger('categories_id');
+            $table->bigInteger('price');
+            $table->longText('description');
+            $table->string('slug');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,10 +17,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345678'),
+            'created_at' => Carbon::now()
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'Pimpinan',
+            'email' => 'pimpinan@example.com',
+            'password' => bcrypt('12345678'),
+            'created_at' => Carbon::now()
+        ]);
+
+        $user->assignRole('pimpinan');
+
+        $user = User::create([
             'name' => 'User',
             'email' => 'user@example.com',
-            'password' => bcrypt('12345678')
+            'password' => bcrypt('12345678'),
+            'created_at' => Carbon::now()
         ]);
+
+        $user->assignRole('user');
     }
 }
