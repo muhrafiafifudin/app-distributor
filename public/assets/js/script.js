4c033171fa2a31e9e06d6d8bb9eeee4aeb479aa4
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-07-13 06:57:37 +0700

    Update outgoing item [Bug]

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,53 +1,53 @@
-// $(document).on("click", ".delete-data", function (e) {
-//     e.preventDefault();
+$(document).on("click", ".delete-data", function (e) {
+    e.preventDefault();
 
-//     var form = $(this).closest("form");
+    var form = $(this).closest("form");
 
-//     Swal.fire({
-//         html: "Yakin untuk menghapus data ??",
-//         icon: "warning",
-//         showCancelButton: true,
-//         confirmButtonText: "Hapus",
-//         cancelButtonText: "Cancel",
-//         customClass: {
-//             confirmButton: "btn btn-danger",
-//             cancelButton: "btn btn-secondary",
-//         },
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             form.submit();
-//         }
-//     });
-// });
-
-$(document).ready(function() {
-    $(".delete-data").click(function (e) {
-        e.preventDefault();
-
-        var item_id = $(this)
-            .closest(".item-data")
-            .find(".item-id")
-            .val();
-
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-        });
-
-        $.ajax({
-            method: "POST",
-            url: "transaksi/delete-item",
-            data: {
-                item_id: item_id,
-            },
-            success: function (response) {
-                // console.log(response);
-                // swal("", response.status, "success").then(function () {
-                // });
-                location.reload();
-            },
-        });
+    Swal.fire({
+        html: "Yakin untuk menghapus data ??",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Hapus",
+        cancelButtonText: "Cancel",
+        customClass: {
+            confirmButton: "btn btn-danger",
+            cancelButton: "btn btn-secondary",
+        },
+    }).then((result) => {
+        if (result.isConfirmed) {
+            form.submit();
+        }
     });
-})
+});
+
+// $(document).ready(function() {
+//     $(".delete-data").click(function (e) {
+//         e.preventDefault();
+
+//         var item_id = $(this)
+//             .closest(".item-data")
+//             .find(".item-id")
+//             .val();
+
+//         $.ajaxSetup({
+//             headers: {
+//                 "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+//             },
+//         });
+
+//         $.ajax({
+//             method: "POST",
+//             url: "transaksi/delete-item",
+//             data: {
+//                 item_id: item_id,
+//             },
+//             success: function (response) {
+//                 // console.log(response);
+//                 // swal("", response.status, "success").then(function () {
+//                 // });
+//                 location.reload();
+//             },
+//         });
+//     });
+// })
 
