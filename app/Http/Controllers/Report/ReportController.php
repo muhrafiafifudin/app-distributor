2031a41bf1164a40c8dab0e7702229d48307dc64
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-07-28 14:14:58 +0700

    Update controller report

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers\Report;
 
+use PDF;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function index()
+    public function incoming_item()
     {
-        return view('pages.report.report');
+        return view('pages.report.incoming_item');
+    }
+
+    public function pdf_print_incoming($fromDate, $toDate)
+    {
+        $pdf = PDF::loadView('pages.report.pdf.incoming_item', compact('fromDate', 'toDate'))->setPaper('a4', 'potrait');
+
+        return $pdf->download('Barang Masukj.pdf');
+    }
+
+    public function outgoing_item()
+    {
+        return view('pages.report.outgoing_item');
+    }
+
+    public function pdf_print_outgoing($fromDate, $toDate)
+    {
+        dd($fromDate, $toDate);
     }
 }
